bb4d75691141f83a12ffa055981418138bdb7e7d
Leo Čuljak <leo.culjak@edu.fit.ba>
2024-03-21 21:04:40 +0100

    Add project files.

---
+++ app/App_Code/Handlers/Blob.generated.cs
@@ -0,0 +1,15 @@
+﻿namespace FreeTrial.Handlers
+{
+
+
+    public partial class BlobFactoryConfig : BlobFactory
+    {
+
+        public static void Initialize()
+        {
+            // register blob handlers
+            RegisterHandler("AttachmentBlobHandler", "\"dbo\".\"Attachments\"", "\"Attachment\"", new string[] {
+                        "\"Id\""}, "Attachments Attachment", "Attachments", "Attachment");
+        }
+    }
+}
